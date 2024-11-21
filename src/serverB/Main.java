aa5a051183742f689ce4de0bdbf4bddc026dedc0
jud-as <guilhermeferreira082@gmail.com>
2024-11-21 17:16:41 -0300

    Estabelecendo conexao entre servidores

--- src/serverB/Main.java
+++ src/serverB/Main.java
@@ -11,11 +11,33 @@ public class Main {
         final String SERVER_A_HOST = "127.0.0.1"; // IP do Servidor A
         final int SERVER_A_PORT = 8080;           // Porta do Servidor A
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) { // Inicializa servidor na porta 8081
             System.out.println("Servidor B está escutando na porta " + PORT);
 
-            try (Socket socket = new Socket(SERVER_A_HOST, SERVER_A_PORT)) {
+
+
+            try (Socket socket = new Socket(SERVER_A_HOST, SERVER_A_PORT)) { // Conecta ao servidor A
                 System.out.println("Conectado ao Servidor " + SERVER_A_HOST + " : " + SERVER_A_PORT);
+
+                // Streams para comunicação:
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true); // Enviar dados para o servidor
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Receber dados do servidor
+
+                // Thread para escutar mensagens do servidor
+                Thread serverListener = new Thread(() -> {
+                    try {
+                        String serverResponse;
+                        while ((serverResponse = in.readLine()) != null) {
+                            System.out.println("Servidor: " + serverResponse);
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Conexão com o servidor encerrada.");
+                    }
+                });
+
+
+                serverListener.start();
+
 
             } catch (IOException e) {
                 System.err.println("Erro na comunicação com o servidor: " + e.getMessage());
